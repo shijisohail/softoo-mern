39e91382bd592cebcc87e673e401f08763fc9f10
shijisohail <shijisohail786@gmail.com>
2022-10-09 17:57:51 +0500

    primary update checking buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Typography } from "@mui/material";
+import { Button } from '@mui/material';
+import LoadingButton from '@mui/lab/LoadingButton';
+import SaveIcon from '@mui/icons-material/Save';
+import React from "react";
+const App = () =>{
+    return(
+        <div>
+            <Typography variant="h1">Hi There</Typography>
+            <Button variant="contained" color = "error">Fuck you!</Button>
+            <LoadingButton loading variant="contained"> Submit</LoadingButton>
+<LoadingButton loading loadingIndicator="Loading…" variant="contained"> Fetch data</LoadingButton>
+<LoadingButton loading loadingPosition="start" startIcon={<SaveIcon />} variant="contained">
+     Save
+</LoadingButton>
+        </div>
+    );
 }
-
 export default App;
